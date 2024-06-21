bbf61032251a5eefc03a5cdf630bd1ff37aa98f1
Yu Chin Fabian Lim <flim@sg.ibm.com>
2024-06-21 07:44:34 +0000

    fix export
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- src/instructlab/dolomite/hf_models/__init__.py
+++ src/instructlab/dolomite/hf_models/__init__.py
@@ -1,2 +1,5 @@
 from .model_conversion import export_to_huggingface, import_from_huggingface
 from .models import GPTDolomiteConfig, GPTDolomiteForCausalLM, GPTDolomiteModel
+from .register_hf import register_model_classes
+
+register_model_classes()
